a5be2eb75060160d71df8f2a92ac98b701caf714
Michelangelo <your.email@example.com>
2025-01-08 12:54:35 +0100

    setup file uploaded

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),  # Automatically find subpackages in `pybit_ms/`
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",  # Minimum Python version
